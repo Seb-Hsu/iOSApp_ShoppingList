a3bac6f1876de9de67672dc30e34550ba52b348b
Sebastian Hsu <sebastianchhsu@icloud.com>
2026-06-10 14:21:53 +0930

    Map View Added nad lauch screen added

--- ShoppingListApp/MapVC.swift
+++ ShoppingListApp/MapVC.swift
@@ -6,13 +6,73 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupLocatonManager()
+        
+    }
+    
+    private func setupLocatonManager() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    // Location Manager func
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        guard let location = locations.last else { return }
+        updateMapRegion(to: location.coordinate)
+        
+    }
+    
+    private func updateMapRegion(to coordinate: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(
+            center: coordinate,
+            latitudinalMeters: 100,
+            longitudinalMeters: 100
+        )
+        mapView.setRegion(region, animated: true)
+    }
+    
+    private func startLocationUpdates(){
+        locationManager.startUpdatingLocation()
+        
+        //Zoom to user location if available
+        if let userLocation = locationManager.location?.coordinate{
+            updateMapRegion(to: userLocation)
+        }
+    }
+    
+    private func handleAuthorizationStatus(_ status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            startLocationUpdates()
+        case .denied, .restricted, .notDetermined:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    // MARK: CLLocationManagerDelegate
+    @available(iOS 14.0, *)
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        handleAuthorizationStatus(manager.authorizationStatus)
     }
     
 
